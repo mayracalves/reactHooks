1b09d2b1248b5f9c9a7e3334937a5905674e9035
Mayra Alves <mayra.alves@dbccompany.com.br>
2020-06-22 16:34:47 -0300

    favoritando repositorios

--- src/App.js
+++ src/App.js
@@ -3,18 +3,34 @@ import React, { useState, useEffect } from 'react';
 export default function App() {
   const [repositories, setRepositories] = useState([]);
 
-  useEffect( async() => {
+  useEffect(async () => {
     const response = await fetch('https://api.github.com/users/mayracalves/repos');
     const data = await response.json();
 
     setRepositories(data);
   }, []);
 
+
+  
+
+  function handleFavorite(id) {
+    const newRepositories = repositories.map(repo => {
+      return repo.id === id ? { ...repo, favorite: !repo.favorite } : repo
+    });
+
+    setRepositories(newRepositories);
+  }
+
   return (
     <ul>
       {repositories.map(repo => (
-        <li key={repo.id}>{repo.name}</li>
+        <li key={repo.id}>
+          {repo.name}
+          <button onClick={() => handleFavorite(repo.id)}>Favoritar</button>
+          {repo.favorite && <span>(Favorito)</span>}
+        </li>
       ))}
+
     </ul>
 
   );
